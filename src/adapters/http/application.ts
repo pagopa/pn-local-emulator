24ff3ec285e36e362ea3de93962eeeb46218970f
AF <alessandro.ferlin@pagopa.it>
2022-08-01 16:27:35 +0200

    Add the http adapter

---
+++ src/adapters/http/application.ts
@@ -0,0 +1,19 @@
+import * as http from "http";
+import express from "express";
+import { Config } from "../../config";
+import { Logger } from "../../logger";
+
+export const makeApplication = (): express.Application => {
+  const app = express();
+  // create all routers and return the application
+  return app;
+};
+
+export const startApplication = (logger: Logger, config: Config, application: express.Application) => {
+  const server = http.createServer(application);
+  const [hostname, port] = [config.server.hostname, config.server.port]
+  server.listen(port, hostname, () => {
+    logger.info(`Server is listening on port ${port}`);
+  });
+
+}
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,21 @@
-console.log("I'm working!");
+import * as E from "fp-ts/Either";
+import { pipe } from "fp-ts/lib/function";
+import * as http from "./adapters/http/application";
+import { parseConfig } from "./config";
+import { makeLogger } from "./logger";
+
+pipe(
+  parseConfig(process.env),
+  E.map((config) => {
+    const logger = makeLogger();
+    // create the driving adapters
+    const application = http.makeApplication()
+    http.startApplication(logger, config, application)
+  }),
+  E.fold(
+    (error) =>
+      console.log(error),
+    (_) =>
+      _
+  )
+)
