dfa1bdba554f3001f5d7d98bea113f3fac6e102a
Marco Comi <marco.comi@pagopa.it>
2022-08-05 16:00:36 +0200

    Inject use case to application

--- src/adapters/http/application.ts
+++ src/adapters/http/application.ts
@@ -6,16 +6,20 @@ import { PreLoadUseCase } from '../../useCases/PreLoadUseCase';
 import { UploadToS3UseCase } from '../../useCases/UploadToS3UseCase';
 import { makePreLoadRouter } from './preLoad/router';
 import { makeUploadToS3Router } from './uploadToS3/router';
+import { SendNotificationUseCase } from '../../useCases/SendNotificationUseCase';
+import { makeSendNotificationRouter } from './sendNotification/router';
 
 export const makeApplication = (
   preLoadUseCase: PreLoadUseCase,
   uploadToS3UseCase: UploadToS3UseCase,
+  sendNotificationUseCase: SendNotificationUseCase
 ): express.Application => {
   const app = express();
   app.use(express.json());
   // create all routers and return the application
   app.use(makePreLoadRouter(preLoadUseCase));
   app.use(makeUploadToS3Router(uploadToS3UseCase));
+  app.use(makeSendNotificationRouter(sendNotificationUseCase));
   return app;
 };
 
