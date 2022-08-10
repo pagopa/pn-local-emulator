1cbece8dfd8cc99ec6ef19613699025be1ae8227
Marco Comi <marco.comi@pagopa.it>
2022-08-10 18:02:25 +0200

    Export checks

--- src/domain/checklist/preLoadChecklist.ts
+++ src/domain/checklist/preLoadChecklist.ts
@@ -1,18 +1,19 @@
 import { pipe } from 'fp-ts/lib/function';
 import * as s from 'fp-ts/string';
 import * as RA from 'fp-ts/ReadonlyArray';
+import * as O from 'fp-ts/Option';
 import { PreLoadRecord } from '../PreLoadRepository';
 import { Checklist } from './types';
 
-const check0 = {
+export const check0 = {
   name: 'Exists a response with status code 401',
   eval: RA.some((record: PreLoadRecord) => record.output.statusCode === 401),
 };
-const check1 = {
+export const check1 = {
   name: 'Contains an api-key',
   eval: RA.some((record: PreLoadRecord) => record.input.apiKey !== undefined),
 };
-const check2 = {
+export const check2 = {
   name: 'preloadIdx values are unique within the body',
   eval: RA.some((record: PreLoadRecord) =>
     pipe(
@@ -23,7 +24,7 @@ const check2 = {
     )
   ),
 };
-const check3 = {
+export const check3 = {
   name: "contentType is always 'application/pdf'",
   eval: RA.some((record: PreLoadRecord) =>
     pipe(
