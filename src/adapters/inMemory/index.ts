fa6f2182d3378c7f8594bbcd3460d0cce658683a
Danilo Spinelli <gunzip@users.noreply.github.com>
2022-08-10 14:16:23 +0200

    Revert insert operation in Repository to make it change the input array (#11)
    * revert
    
    * revert repository

--- src/adapters/inMemory/index.ts
+++ src/adapters/inMemory/index.ts
@@ -1,25 +1,28 @@
 import { flow } from 'fp-ts/lib/function';
-import { append } from 'fp-ts/lib/ReadonlyArray';
-import { ReadonlyNonEmptyArray } from 'fp-ts/lib/ReadonlyNonEmptyArray';
 import * as TE from 'fp-ts/TaskEither';
 import { Repository } from '../../domain/Repository';
 import { Logger } from '../../logger';
 
 export const insertEntityTE =
-  <E>(store: ReadonlyArray<E>) =>
-  (entity: E): TE.TaskEither<Error, ReadonlyNonEmptyArray<E>> =>
-    TE.right(append(entity)(store));
+  <E>(store: E[]) =>
+  (entity: E): TE.TaskEither<Error, E> => {
+    // eslint-disable-next-line functional/immutable-data
+    store.push(entity);
+    return TE.right(entity);
+  };
 
 const makeRepository =
   (logger: Logger) =>
-  <T>(store: ReadonlyArray<T>): Repository<T> => ({
-    insert: flow(
-      insertEntityTE(store),
-      TE.chainFirst((item) => TE.of(logger.debug(`Record item: ${item}`)))
-    ),
-    list: () => TE.of(store),
-  });
-
+  <T>(snapshot: ReadonlyArray<T>): Repository<T> => {
+    const store = [...snapshot];
+    return {
+      insert: flow(
+        insertEntityTE(store),
+        TE.chainFirst((item) => TE.of(logger.debug(`Record item: ${item}`)))
+      ),
+      list: () => TE.of(store),
+    };
+  };
 export const makePreLoadRepository = makeRepository;
 
 export const makeUploadToS3Repository = makeRepository;
--- src/domain/Repository.ts
+++ src/domain/Repository.ts
@@ -1,8 +1,7 @@
-import { ReadonlyNonEmptyArray } from 'fp-ts/lib/ReadonlyNonEmptyArray';
 import * as TE from 'fp-ts/TaskEither';
 
 export type Repository<A> = {
-  insert: (input: A) => TE.TaskEither<Error, ReadonlyNonEmptyArray<A>>;
+  insert: (input: A) => TE.TaskEither<Error, A>;
 
   list: () => TE.TaskEither<Error, ReadonlyArray<A>>;
 };
