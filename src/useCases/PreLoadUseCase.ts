1e655b44c2aeabd98df465f75ebe0b2a3fc01de7
danilo spinelli <danilo.spinelli@pagopa.it>
2022-08-14 18:23:16 +0200

    refactor proposal

--- src/useCases/PreLoadUseCase.ts
+++ src/useCases/PreLoadUseCase.ts
@@ -1,6 +1,6 @@
 import * as crypto from 'crypto';
-import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/lib/function';
+import * as TE from 'fp-ts/TaskEither';
 import { Logger } from '../logger';
 import {
   makePreLoadRecord,
@@ -10,26 +10,31 @@ import {
 } from '../domain/PreLoadRepository';
 import { ApiKey } from '../generated/definitions/ApiKey';
 import { PreLoadRequestBody } from '../generated/definitions/PreLoadRequestBody';
+import { PreLoadResponse } from '../generated/definitions/PreLoadResponse';
 import { onValidApiKey } from './utils';
+
+// build response payload from the given request body
+const makeResponsePayload = (url: string, body: PreLoadRequestBody): PreLoadResponse[] =>
+  body.map((req) => makePreLoadResponse(crypto.randomUUID(), crypto.randomUUID(), url, req));
+
+// get a successful response payload or authentication error in case the API key is not valid
+const makeResponse = (apiKey: string) => (returned: PreLoadResponse[]) =>
+  pipe({ statusCode: 200 as const, returned }, onValidApiKey(apiKey), TE.fromEither, TE.toUnion, TE.fromTask);
+
+// log the content of the record inserted into the repository
+const logPreloadRecord = (logger: Logger) => (record: PreLoadRecord) => pipe(record, logger.debug, TE.right);
 
 export const PreLoadUseCase =
   (logger: Logger, uploadToS3URL: URL, repository: PreLoadRecordRepository) =>
   (apiKey: ApiKey) =>
-  (body: PreLoadRequestBody): TE.TaskEither<Error, PreLoadRecord['output']> => {
-    const input = { apiKey, body };
-    const url = uploadToS3URL.href;
-    const returned = body.map((req) => {
-      // TODO: Move into an adapter
-      const [key, secret] = [crypto.randomUUID(), crypto.randomUUID()];
-      return makePreLoadResponse(key, secret, url, req);
-    });
-    const output = onValidApiKey(apiKey)({ statusCode: 200 as const, returned });
-    const record = makePreLoadRecord({ input, output });
-    return pipe(
-      repository.insert(record),
-      TE.chainFirst(() => TE.of(logger.debug(record))),
-      TE.map((_) => record.output)
+  (body: PreLoadRequestBody): TE.TaskEither<Error, PreLoadRecord['output']> =>
+    pipe(
+      TE.of(makeResponsePayload(uploadToS3URL.href, body)),
+      TE.chain(makeResponse(apiKey)),
+      TE.map((output) => makePreLoadRecord({ input: { apiKey, body }, output })),
+      TE.chain(repository.insert),
+      TE.chainFirst(logPreloadRecord(logger)),
+      TE.map((record) => record.output)
     );
-  };
 
 export type PreLoadUseCase = ReturnType<typeof PreLoadUseCase>;
--- src/useCases/utils.ts
+++ src/useCases/utils.ts
@@ -1,12 +1,16 @@
-import * as b from 'fp-ts/boolean';
+import * as E from 'fp-ts/lib/Either';
+import { pipe } from 'fp-ts/lib/function';
 import { ApiKey } from '../generated/definitions/ApiKey';
 import { Response } from '../domain/types';
 
 // TODO: Get "key-value" from repository or configuration?
 export const onValidApiKey =
   (apiKey: ApiKey) =>
-  <T>(valid: T): T | Response<401> =>
-    b.foldW(
-      () => ({ statusCode: 401 as const, returned: undefined }),
-      () => valid
-    )(apiKey === 'key-value');
+  <S extends 200 | 202, T>(valid: Response<S, T>) =>
+    pipe(
+      valid,
+      E.fromPredicate(
+        () => apiKey === 'key-value',
+        () => ({ statusCode: 401 as const, returned: undefined })
+      )
+    );
