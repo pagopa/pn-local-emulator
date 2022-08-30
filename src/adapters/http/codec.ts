424704b3946faf819c71401b7f77a88c8df013c6
AF <alessandro.ferlin@pagopa.it>
2022-08-30 14:47:52 +0200

    Removes code duplication

--- src/adapters/http/codec.ts
+++ src/adapters/http/codec.ts
@@ -3,12 +3,9 @@ import * as PR from 'io-ts/PathReporter';
 import { Problem } from '../../generated/definitions/Problem';
 
 export const makeProblemFromErrors = (errors: ts.Errors): Problem => ({
-  type: `https://www.webfx.com/web-development/glossary/http-status-codes/what-is-a-400-status-code/`,
-  // TODO: fix this bad cast
-  status: 400 as null,
-  title: 'Bad input',
+  ...makeProblem(400),
+  title: 'Bad Request',
   detail: PR.failure(errors),
-  errors: [],
 });
 
 export const makeProblem = (statusCode: number): Problem => ({
