dbb22a0e24ffb82fab64b7cce92d3420b7378b79
AF <alessandro.ferlin@pagopa.it>
2022-08-31 09:22:07 +0200

    Fix compilation error on checklist router

--- src/adapters/http/checklist/router.ts
+++ src/adapters/http/checklist/router.ts
@@ -1,25 +1,30 @@
 import express from 'express';
 import { pipe } from 'fp-ts/lib/function';
 import * as E from 'fp-ts/Either';
-import * as utils from '../utils';
+import * as T from 'fp-ts/Task';
+import * as TE from 'fp-ts/TaskEither';
+import * as Problem from '../Problem';
 import { GetChecklistResultUseCase } from '../../../useCases/GetChecklistResultUseCase';
+import { Handler, toExpressHandler } from '../Handler';
 import * as codec from './codec';
 
 const getChecklistResultHandler =
-  (getChecklistResultUseCase: GetChecklistResultUseCase): express.Handler =>
+  (getChecklistResultUseCase: GetChecklistResultUseCase): Handler =>
   (_, res) =>
     pipe(
       E.of(getChecklistResultUseCase()),
-      utils.sendResponse(res)(
-        (_) => res.status(500).send(utils.makeAPIProblem(500, 'Internal Server Error')(undefined)),
-        (result) => res.status(200).send(codec.makeChecklistResult(result))
+      E.map(
+        TE.fold(
+          (_) => T.of(res.status(500).send(Problem.fromNumber(500))),
+          (result) => T.of(res.status(200).send(codec.makeChecklistResult(result)))
+        )
       )
     );
 
 export const makeChecklistRouter = (getChecklistResultUseCase: GetChecklistResultUseCase): express.Router => {
   const router = express.Router();
 
-  router.get('/checklistresult', getChecklistResultHandler(getChecklistResultUseCase));
+  router.get('/checklistresult', toExpressHandler(getChecklistResultHandler(getChecklistResultUseCase)));
 
   return router;
 };
