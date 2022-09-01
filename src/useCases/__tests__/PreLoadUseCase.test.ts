75adb8084e980ccfcd50e473586215e07f7a8b25
AF <alessandro.ferlin@pagopa.it>
2022-09-01 09:43:53 +0200

    Apply suggestion from the code-review

--- src/useCases/__tests__/PreLoadUseCase.test.ts
+++ src/useCases/__tests__/PreLoadUseCase.test.ts
@@ -6,28 +6,29 @@ import { config } from '../../__tests__/data';
 import * as inMemory from '../../adapters/inMemory';
 import { pipe } from 'fp-ts/lib/function';
 
+// TODO: Remove this hardcoded value; the valid api-key at some point will be taken from envs
+const apiKey = 'key-value';
 const logger = makeLogger();
 const repository = inMemory.makeRepository(logger)([]);
 const body = [
   {
     preloadIdx: '0',
     contentType: 'application/pdf',
-    sha256: 'xxx',
+    sha256: 'sha-value',
   },
 ];
 
 describe('PreLoadUseCase', () => {
   it('should return the key into url for each elements', async () => {
-    const [apiKey] = ['key-value'];
     const actual = await PreLoadUseCase(logger, config.server.uploadToS3URL, repository)(apiKey)(body)();
     const checkKey = pipe(
       actual,
       E.exists(
-        (e) =>
-          e.statusCode === 200 &&
+        (element) =>
+          element.statusCode === 200 &&
           pipe(
-            e.returned,
-            RA.every((e) => e.url.endsWith(e.key))
+            element.returned,
+            RA.every((_) => _.url.endsWith(_.key))
           )
       )
     );
