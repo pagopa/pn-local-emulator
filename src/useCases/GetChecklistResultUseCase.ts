bca13ccbd8674fddd98e6afab7780ef23004efaf
AF <alessandro.ferlin@pagopa.it>
2022-09-06 16:32:45 +0200

    Replace checklistConcat with explicit type binding

--- src/useCases/GetChecklistResultUseCase.ts
+++ src/useCases/GetChecklistResultUseCase.ts
@@ -2,12 +2,10 @@ import { pipe } from 'fp-ts/lib/function';
 import * as RA from 'fp-ts/ReadonlyArray';
 import * as TE from 'fp-ts/TaskEither';
 import { preLoadChecklist } from '../domain/checklist/preLoadChecklist';
-import { ChecklistResult, evalChecklist } from '../domain/checklist/types';
+import { ChecklistResult, evalChecklist, Result } from '../domain/checklist/types';
 import { uploadToS3Checklist } from '../domain/checklist/uploadToS3Checklist';
 import { PreLoadRecordRepository } from '../domain/PreLoadRepository';
 import { UploadToS3RecordRepository } from '../domain/UploadToS3RecordRepository';
-
-const checklistConcat = (l: ChecklistResult) => (r: ChecklistResult) => RA.concat(l)(r);
 
 export const GetChecklistResultUseCase =
   (preLoadRecordRepository: PreLoadRecordRepository, uploadToS3RecordRepository: UploadToS3RecordRepository) =>
@@ -19,10 +17,10 @@ export const GetChecklistResultUseCase =
       TE.ap(uploadToS3RecordRepository.list()),
       TE.map(evalChecklist(uploadToS3Checklist))
     );
-    // TODO: Figure out how improve this part, the pipe should be writtable as following
+    // TODO: Figure out how to improve this part, the pipe should be writtable as following
     // pipe(TE.of(RA.concat), TE.ap(preLoadChecklistResult), TE.ap(uploadToS3ChecklistResult));
-    // without the need of the checklistConcat function
-    return pipe(TE.of(checklistConcat), TE.ap(preLoadChecklistResult), TE.ap(uploadToS3ChecklistResult));
+    // without the need to explicit bind the type of RA.contact
+    return pipe(TE.of(RA.concat<Result>), TE.ap(preLoadChecklistResult), TE.ap(uploadToS3ChecklistResult));
   };
 
 export type GetChecklistResultUseCase = ReturnType<typeof GetChecklistResultUseCase>;
