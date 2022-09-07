5b0271955c88a06ee88a700cdac78c13f2f4655e
Marco Comi <9998393+kin0992@users.noreply.github.com>
2022-09-07 15:14:56 +0200

    Add method which strips null properties

--- src/adapters/http/Handler.ts
+++ src/adapters/http/Handler.ts
@@ -4,6 +4,7 @@ import * as T from 'fp-ts/Task';
 import * as E from 'fp-ts/Either';
 import { pipe } from 'fp-ts/function';
 import * as Problem from './Problem';
+import { isObject } from '@pagopa/ts-commons/lib/types';
 
 export type Handler = (req: express.Request, res: express.Response) => t.Validation<T.Task<express.Response>>;
 
@@ -16,3 +17,19 @@ export const toExpressHandler =
       E.toUnion,
       (task) => task()
     );
+
+/**
+ * Return an object filtering out keys that point to null values.
+ */
+export const withoutNullValues = <T, K extends keyof T>(obj: T): T => {
+  const keys = Object.keys(obj);
+  return keys.reduce((acc, key) => {
+    const value = obj[key as K];
+    return value !== null
+      ? {
+          ...acc,
+          [key]: isObject(value) ? withoutNullValues(value) : value,
+        }
+      : acc;
+  }, {} as T);
+};
