46bfd9d595cd838040d9f4d40f35372a3783ada7
Marco Comi <9998393+kin0992@users.noreply.github.com>
2022-09-08 17:11:08 +0200

    Add unit test

--- src/adapters/http/__tests__/Handler.test.ts
+++ src/adapters/http/__tests__/Handler.test.ts
@@ -1,23 +1,30 @@
 import { removeNullValues } from '../Handler';
 
-describe('withoutNullValues', () => {
-  it('should filter out null properties recursively', async () => {
-    const obj = {
-      a: 1,
-      b: null,
-      c: {
-        d: [1, 2],
-        e: null,
-      },
-    };
-    const actual = removeNullValues(obj);
-    const expected = {
-      a: 1,
-      c: {
-        d: [1, 2],
-      },
-    };
+describe('removeNullValues', () => {
+  const inputObject = {
+    a: 1,
+    b: null,
+    c: {
+      d: [1, 2],
+      e: null,
+    },
+    f: [{ g: null, h: 1 }],
+  };
+  const expected = {
+    a: 1,
+    c: {
+      d: [1, 2],
+    },
+    f: [{ h: 1 }],
+  };
 
+  it('should filter out null properties recursively', async () => {
+    const actual = removeNullValues(inputObject);
+    expect(actual).toStrictEqual(expected);
+  });
+
+  it('should filter out null properties recursively - input is an array', async () => {
+    const actual = removeNullValues([inputObject]);
     expect(actual).toStrictEqual(expected);
   });
 });
