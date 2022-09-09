12fcb40221c586423863533171214798c7b66f6c
Marco Comi <9998393+kin0992@users.noreply.github.com>
2022-09-09 08:11:42 +0200

    Fix unit test

--- src/adapters/http/__tests__/Handler.test.ts
+++ src/adapters/http/__tests__/Handler.test.ts
@@ -8,14 +8,14 @@ describe('removeNullValues', () => {
       d: [1, 2],
       e: null,
     },
-    f: [{ g: null, h: 1 }],
+    f: [{ g: 1 }, { h: null }],
   };
   const expected = {
     a: 1,
     c: {
       d: [1, 2],
     },
-    f: [{ h: 1 }],
+    f: [{ g: 1 }],
   };
 
   it('should filter out null properties recursively', async () => {
@@ -25,6 +25,6 @@ describe('removeNullValues', () => {
 
   it('should filter out null properties recursively - input is an array', async () => {
     const actual = removeNullValues([inputObject]);
-    expect(actual).toStrictEqual(expected);
+    expect(actual).toStrictEqual([expected]);
   });
 });
