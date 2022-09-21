8cf4ca9442fe072e6bfcb5dbebe0a208d232fb81
Marco Comi <9998393+kin0992@users.noreply.github.com>
2022-09-21 11:05:27 +0200

    Change implementaion of authorizeApiKey method according to the server behaviour

--- src/domain/authorize.ts
+++ src/domain/authorize.ts
@@ -1,14 +1,18 @@
 import * as E from 'fp-ts/lib/Either';
 import { pipe } from 'fp-ts/lib/function';
 import { ApiKey } from '../generated/definitions/ApiKey';
-import { Response } from './types';
+import { UnauthorizedMessageBody, Response } from './types';
 
-export const authorizeApiKey = (apiKey: ApiKey): E.Either<Response<401>, ApiKey> =>
+export const unauthorizedMessage: UnauthorizedMessageBody = {
+  message: 'User is not authorized to access this resource with an explicit deny',
+};
+
+export const authorizeApiKey = (apiKey: ApiKey): E.Either<Response<403, UnauthorizedMessageBody>, ApiKey> =>
   pipe(
     apiKey,
     E.fromPredicate(
       // TODO: Get "key-value" from configuration
       () => apiKey === 'key-value',
-      () => ({ statusCode: 401 as const, returned: undefined })
+      () => ({ statusCode: 403 as const, returned: unauthorizedMessage })
     )
   );
