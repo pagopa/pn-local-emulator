c36beab81d687f8b2d36db0c8bcc7c18c2a4ef12
Marco Comi <9998393+kin0992@users.noreply.github.com>
2022-09-22 16:09:01 +0200

    [VL-67] - Add timestamp and traceId to error response (#33)

--- src/adapters/http/Problem.ts
+++ src/adapters/http/Problem.ts
@@ -6,6 +6,9 @@ export const fromErrors = (errors: ts.Errors): Problem => ({
   ...fromNumber(400),
   title: 'Bad Request',
   detail: PR.failure(errors),
+  timestamp: new Date(),
+  traceId:
+    'Self=1-631b10db-2c591f3f79954187229939e5;Root=1-631b11db-39caf6636f4ed15618461d95;Parent=6e2ef00e703981ac;Sampled=1',
 });
 
 export const fromNumber = (statusCode: number): Problem => ({
