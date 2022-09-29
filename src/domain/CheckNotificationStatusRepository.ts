3de49ec72440f781fbd15386ae63766319e2006c
AF <4775679+datalek@users.noreply.github.com>
2022-09-29 11:03:03 +0200

    [VL-93] Change check notification status endpoint (#38)
    Before this commit, the check notification status endpoint always returned the notification in the WAITING state.
    After this commit, the endpoint returns the notification with a different state. The new returned status is based on how many times the endpoint was called.

--- src/domain/CheckNotificationStatusRepository.ts
+++ src/domain/CheckNotificationStatusRepository.ts
@@ -1,11 +1,13 @@
+import { pipe } from 'fp-ts/function';
 import * as O from 'fp-ts/Option';
+import * as RA from 'fp-ts/ReadonlyArray';
 import { NewNotificationRequestStatusResponse } from '../generated/definitions/NewNotificationRequestStatusResponse';
 import { PaProtocolNumber } from '../generated/definitions/PaProtocolNumber';
 import { IdempotenceToken } from '../generated/definitions/IdempotenceToken';
 import { NotificationRequestId } from '../generated/definitions/NotificationRequestId';
 import { Repository } from './Repository';
 import { Response, UnauthorizedMessageBody } from './types';
-import { NewNotificationRecord } from './NewNotificationRepository';
+import { Notification } from './NewNotificationRepository';
 
 export type CheckNotificationStatusRecord = {
   type: 'CheckNotificationStatusRecord';
@@ -15,20 +17,37 @@ export type CheckNotificationStatusRecord = {
   output: Response<200, NewNotificationRequestStatusResponse> | Response<403, UnauthorizedMessageBody> | Response<404>;
 };
 
-export const makeNewNotificationRequestStatusResponse = (
-  record: NewNotificationRecord
-): O.Option<NewNotificationRequestStatusResponse> => {
-  // eslint-disable-next-line sonarjs/no-small-switch
-  switch (record.output.statusCode) {
-    case 202:
-      return O.some({
-        ...record.output.returned,
-        ...record.input.body,
-        notificationRequestStatus: '__WAITING__',
-      });
-    default:
-      return O.none;
-  }
-};
+const getNewNotificationRequestStatusResponse = (
+  record: CheckNotificationStatusRecord
+): O.Option<NewNotificationRequestStatusResponse> =>
+  record.output.statusCode === 200 ? O.some(record.output.returned) : O.none;
+
+export const getNotificationStatusList = RA.filterMap(getNewNotificationRequestStatusResponse);
+
+const WAITING = 'WAITING';
+
+export const makeNewNotificationRequestStatusResponse =
+  (minNumberOfWaitingBeforeDelivering: number, iunGenerator: () => string) =>
+  (responseList: ReadonlyArray<NewNotificationRequestStatusResponse>) =>
+  (notification: Notification): NewNotificationRequestStatusResponse =>
+    pipe(
+      responseList,
+      // find all responses that match the given notification
+      RA.filter(({ notificationRequestId: id }) => id === notification.notificationRequestId),
+      (list) =>
+        pipe(
+          list,
+          // if a non pending response already exists then return it
+          RA.findLast(({ notificationRequestStatus }) => notificationRequestStatus !== WAITING),
+          // otherwise create a new response
+          O.getOrElse(() => ({ ...notification, notificationRequestStatus: WAITING })),
+          (response) =>
+            // if the resource was requested more times
+            // than the threshold then return it as completed
+            list.length >= minNumberOfWaitingBeforeDelivering
+              ? { ...response, notificationRequestStatus: 'ACCEPTED', iun: iunGenerator() }
+              : response
+        )
+    );
 
 export type CheckNotificationStatusRecordRepository = Repository<CheckNotificationStatusRecord>;
