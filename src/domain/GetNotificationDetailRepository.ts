71ef686a7283040ccae950d257116f8949ce19b7
Marco Comi <9998393+kin0992@users.noreply.github.com>
2022-10-03 16:33:06 +0200

    Implements repository

--- src/domain/GetNotificationDetailRepository.ts
+++ src/domain/GetNotificationDetailRepository.ts
@@ -1,8 +1,11 @@
 import { ApiKey } from '../generated/definitions/ApiKey';
 import { Iun } from '../generated/definitions/Iun';
 import { FullSentNotification } from '../generated/definitions/FullSentNotification';
-import { Response, UnauthorizedMessageBody } from './types';
+import { NotificationStatusEnum } from '../generated/definitions/NotificationStatus';
+import { PhysicalCommunicationTypeEnum } from '../generated/definitions/NewNotificationRequest';
 import { Repository } from './Repository';
+import { Response, UnauthorizedMessageBody } from './types';
+import { Notification } from './Notification';
 
 export type GetNotificationDetailRecord = {
   type: 'GetNotificationDetailRecord';
@@ -11,3 +14,20 @@ export type GetNotificationDetailRecord = {
 };
 
 export type GetNotificationDetailRecordRepository = Repository<GetNotificationDetailRecord>;
+
+// TODO: Improve this method
+export const makeFullSentNotification = (notification: Notification): FullSentNotification => ({
+  ...notification,
+  // Momento di ricezione della notifica da parte di PN
+  sentAt: new Date(),
+  notificationStatus: NotificationStatusEnum.ACCEPTED,
+  // elenco degli avanzamenti effettuati dal processo di notifica
+  notificationStatusHistory: [],
+  // Indica se i documenti notificati sono ancora disponibili.
+  documentsAvailable: true,
+  cancelledByIun: 'cancelledByIun',
+  // elenco dettagliato di tutto ciò che è accaduto durrante il processo di notifica
+  timeline: [],
+  // Identificativo IPA della PA mittente che ha eseguito l'onborading su SelfCare.
+  senderPaId: 'senderPaId',
+});
