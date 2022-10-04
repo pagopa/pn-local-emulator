094848b30c57f0906b2c24c6209cd215b7ab2ef6
Marco Comi <9998393+kin0992@users.noreply.github.com>
2022-10-04 16:09:31 +0200

    Fix makeFullSentNotification method

--- src/domain/GetNotificationDetailRepository.ts
+++ src/domain/GetNotificationDetailRepository.ts
@@ -1,8 +1,11 @@
+import { pipe } from 'fp-ts/function';
 import { ApiKey } from '../generated/definitions/ApiKey';
 import { Iun } from '../generated/definitions/Iun';
 import { FullSentNotification } from '../generated/definitions/FullSentNotification';
 import { NotificationStatusEnum } from '../generated/definitions/NotificationStatus';
-import { PhysicalCommunicationTypeEnum } from '../generated/definitions/NewNotificationRequest';
+import { NotificationStatusHistoryElement } from '../generated/definitions/NotificationStatusHistoryElement';
+import { TimelineElementCategoryEnum } from '../generated/definitions/TimelineElementCategory';
+import { TimelineElement } from '../generated/definitions/TimelineElement';
 import { Repository } from './Repository';
 import { Response, UnauthorizedMessageBody } from './types';
 import { Notification } from './Notification';
@@ -15,19 +18,39 @@ export type GetNotificationDetailRecord = {
 
 export type GetNotificationDetailRecordRepository = Repository<GetNotificationDetailRecord>;
 
-// TODO: Improve this method
-export const makeFullSentNotification = (notification: Notification): FullSentNotification => ({
-  ...notification,
-  // Momento di ricezione della notifica da parte di PN
-  sentAt: new Date(),
-  notificationStatus: NotificationStatusEnum.ACCEPTED,
-  // elenco degli avanzamenti effettuati dal processo di notifica
-  notificationStatusHistory: [],
-  // Indica se i documenti notificati sono ancora disponibili.
-  documentsAvailable: true,
-  cancelledByIun: 'cancelledByIun',
-  // elenco dettagliato di tutto ciò che è accaduto durrante il processo di notifica
-  timeline: [],
-  // Identificativo IPA della PA mittente che ha eseguito l'onborading su SelfCare.
-  senderPaId: 'senderPaId',
+const makeTimelineElementId = (iun: string) => `${iun}_request_accepted`;
+
+const makeNotificationStatusHistoryElement = (
+  iun: Iun,
+  status: NotificationStatusEnum,
+  activeFrom: Date
+): NotificationStatusHistoryElement => ({
+  status,
+  activeFrom,
+  relatedTimelineElements: [makeTimelineElementId(iun)],
 });
+
+const makeTimelineElement = (elementId: string): TimelineElement => ({
+  elementId,
+  category: TimelineElementCategoryEnum.REQUEST_ACCEPTED,
+});
+
+export const makeFullSentNotification =
+  (senderPaId: string) =>
+  (sentAt: Date) =>
+  (notification: Notification): FullSentNotification => ({
+    ...notification,
+    sentAt,
+    // https://pagopa.atlassian.net/browse/VL-99
+    notificationStatus: NotificationStatusEnum.ACCEPTED,
+    // https://pagopa.atlassian.net/browse/VL-102
+    notificationStatusHistory: [
+      makeNotificationStatusHistoryElement(notification.iun, NotificationStatusEnum.ACCEPTED, sentAt),
+    ],
+    // https://pagopa.atlassian.net/browse/VL-100
+    documentsAvailable: true,
+    // https://pagopa.atlassian.net/browse/VL-103
+    timeline: [pipe(notification.iun, makeTimelineElementId, makeTimelineElement)],
+    // https://pagopa.atlassian.net/browse/VL-101
+    senderPaId,
+  });
