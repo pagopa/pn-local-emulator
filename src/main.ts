e9decd6e4da6f3bce5bf1990193150315507be5a
Marco Comi <9998393+kin0992@users.noreply.github.com>
2022-10-14 14:47:59 +0200

    Inject SystemEnv to UploadToS3UseCase

--- src/main.ts
+++ src/main.ts
@@ -59,7 +59,7 @@ pipe(
 
     /* init the use cases */
     const preLoadUseCase = PreLoadUseCase(config.server.uploadToS3URL, preLoadRecordRepository);
-    const uploadToS3UseCase = UploadToS3UseCase(uploadToS3RecordRepository);
+    const uploadToS3UseCase = UploadToS3UseCase(systemEnv);
     const sendNotificationUseCase = SendNotificationUseCase(newNotificationRepository);
     const createEventStreamUseCase = CreateEventStreamUseCase(createEventStreamRecordRepository);
     const checkNotificationStatusUseCase = CheckNotificationStatusUseCase(systemEnv);
