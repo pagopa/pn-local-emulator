4937115fe6831175c2135b387388da56a817c468
Marco Comi <9998393+kin0992@users.noreply.github.com>
2022-10-18 09:55:56 +0200

    Add checks

--- src/domain/checklist/checkNotificationStatusChecklist.ts
+++ src/domain/checklist/checkNotificationStatusChecklist.ts
@@ -8,11 +8,27 @@ export const check0 = {
   eval: RA.some((record: CheckNotificationStatusRecord) => record.output.statusCode === 200),
 };
 
+export const check1 = {
+  name: 'Exist a request that returns 200 as status code and the "ACCEPTED" as notification request status',
+  eval: RA.some(
+    (record: CheckNotificationStatusRecord) =>
+      record.output.statusCode === 200 && record.output.returned.notificationRequestStatus === 'ACCEPTED'
+  ),
+};
+
+export const check2 = {
+  name: 'Exist a request that returns 200 as status code and the "WAITING" as notification request status',
+  eval: RA.some(
+    (record: CheckNotificationStatusRecord) =>
+      record.output.statusCode === 200 && record.output.returned.notificationRequestStatus === 'WAITING'
+  ),
+};
+
 const group = {
   name: 'Check notification status request',
 };
 
 export const checkNotificationStatusChecklist: Checklist<ReadonlyArray<CheckNotificationStatusRecord>> = pipe(
-  [check0],
+  [check0, check1, check2],
   RA.map((check) => ({ ...check, group }))
 );
