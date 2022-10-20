459ea6b9eb00c3ab63384d1076ec5d001d1a4695
Marco Comi <9998393+kin0992@users.noreply.github.com>
2022-10-20 16:59:36 +0200

    Cleanup GetCheclistResultUseCase

--- src/useCases/GetChecklistResultUseCase.ts
+++ src/useCases/GetChecklistResultUseCase.ts
@@ -2,9 +2,7 @@ import { pipe } from 'fp-ts/lib/function';
 import * as Apply from 'fp-ts/Apply';
 import * as RA from 'fp-ts/ReadonlyArray';
 import * as TE from 'fp-ts/TaskEither';
-import { preLoadChecklist } from '../domain/checklist/preLoadChecklist';
 import { ChecklistResult, evalChecklist } from '../domain/checklist/types';
-import { uploadToS3Checklist } from '../domain/checklist/uploadToS3Checklist';
 import { sendPaymentNotificationChecklist } from '../domain/checklist/sendPaymentNotificationChecklist';
 import { SystemEnv } from './SystemEnv';
 
@@ -18,20 +16,14 @@ export const GetChecklistResultUseCase =
         createNotificationRequestList: createNotificationRequestRecordRepository.list(),
       }),
       TE.map(({ preLoadList, uploadList, createNotificationRequestList }) => ({
-        preLoadChecklistIn: preLoadList,
-        uploadChecklistIn: RA.concatW(preLoadList)(uploadList),
         sendPaymentNotificationChecklistIn: pipe(
           preLoadList,
           RA.concatW(uploadList),
           RA.concatW(createNotificationRequestList)
         ),
       })),
-      TE.map(({ preLoadChecklistIn, uploadChecklistIn, sendPaymentNotificationChecklistIn }) =>
-        pipe(
-          evalChecklist(preLoadChecklist)(preLoadChecklistIn),
-          RA.concat(evalChecklist(uploadToS3Checklist)(uploadChecklistIn)),
-          RA.concat(evalChecklist(sendPaymentNotificationChecklist)(sendPaymentNotificationChecklistIn))
-        )
+      TE.map(({ sendPaymentNotificationChecklistIn }) =>
+        evalChecklist(sendPaymentNotificationChecklist)(sendPaymentNotificationChecklistIn)
       )
     );
 
