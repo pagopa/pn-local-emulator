ad1f1fb72bb3e7604578d80e5095615b9d54bc48
Marco Comi <9998393+kin0992@users.noreply.github.com>
2022-10-20 17:11:01 +0200

    Replace string with ApiKey type

--- src/domain/Repository.ts
+++ src/domain/Repository.ts
@@ -1,6 +1,7 @@
 import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
 import * as O from 'fp-ts/Option';
+import { ApiKey } from '../generated/definitions/ApiKey';
 import { NewNotificationRecord } from './NewNotificationRepository';
 import { PreLoadRecord } from './PreLoadRepository';
 import { UploadToS3Record } from './UploadToS3RecordRepository';
@@ -14,5 +15,5 @@ export type Repository<A> = {
 // TODO: Add missing records or find another solution
 export type AllRecord = PreLoadRecord | UploadToS3Record | NewNotificationRecord;
 
-export const existsApiKey = <T extends { input: { apiKey: string } }>(record: T) =>
+export const existsApiKey = <T extends { input: { apiKey: ApiKey } }>(record: T) =>
   pipe(record.input.apiKey, O.fromNullable, O.isSome);
