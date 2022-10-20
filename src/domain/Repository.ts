16f88e18c1a36832b77d591af4f234c4adc70e76
Marco Comi <9998393+kin0992@users.noreply.github.com>
2022-10-20 16:36:02 +0200

    Create existApiKey predicate

--- src/domain/Repository.ts
+++ src/domain/Repository.ts
@@ -1,4 +1,6 @@
 import * as TE from 'fp-ts/TaskEither';
+import { pipe } from 'fp-ts/function';
+import * as O from 'fp-ts/Option';
 import { NewNotificationRecord } from './NewNotificationRepository';
 import { PreLoadRecord } from './PreLoadRepository';
 import { UploadToS3Record } from './UploadToS3RecordRepository';
@@ -11,3 +13,6 @@ export type Repository<A> = {
 
 // TODO: Add missing records or find another solution
 export type AllRecord = PreLoadRecord | UploadToS3Record | NewNotificationRecord;
+
+export const existsApiKey = <T extends { input: { apiKey: string } }>(record: T) =>
+  pipe(record.input.apiKey, O.fromNullable, O.isSome);
