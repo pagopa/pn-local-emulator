4d61d690e0d81266d4bcb6278873c4e9bca3be80
Marco Comi <9998393+kin0992@users.noreply.github.com>
2022-10-24 14:49:18 +0200

    Add conditions to verify for send notification

--- src/domain/NewNotificationRepository.ts
+++ src/domain/NewNotificationRepository.ts
@@ -1,9 +1,10 @@
 import * as O from 'fp-ts/Option';
 import * as RA from 'fp-ts/ReadonlyArray';
+import { pipe } from 'fp-ts/lib/function';
 import { ApiKey } from '../generated/definitions/ApiKey';
-import { NewNotificationRequest } from '../generated/definitions/NewNotificationRequest';
+import { NewNotificationRequest, PhysicalCommunicationTypeEnum } from '../generated/definitions/NewNotificationRequest';
 import { NewNotificationResponse } from '../generated/definitions/NewNotificationResponse';
-import { Repository } from './Repository';
+import { AllRecord, Repository } from './Repository';
 import { Response, UnauthorizedMessageBody } from './types';
 
 export type Notification = NewNotificationRequest & NewNotificationResponse;
@@ -13,6 +14,58 @@ export type NewNotificationRecord = {
   input: { apiKey: ApiKey; body: NewNotificationRequest };
   output: Response<202, NewNotificationResponse> | Response<403, UnauthorizedMessageBody>;
 };
+
+export const isNewNotificationRecord = (record: AllRecord): O.Option<NewNotificationRecord> =>
+  record.type === 'NewNotificationRecord' ? O.some(record) : O.none;
+
+export const hasSuccessfulResponse = (record: NewNotificationRecord) => record.output.statusCode === 202;
+
+export const hasRecipientTaxId = (record: NewNotificationRecord) =>
+  pipe(
+    record.input.body.recipients,
+    RA.every((recipient) => pipe(recipient.taxId, O.fromNullable, O.isSome))
+  );
+
+export const hasRecipientDigitalDomicile = (record: NewNotificationRecord) =>
+  pipe(
+    record.input.body.recipients,
+    RA.every((recipient) => pipe(recipient.digitalDomicile, O.fromNullable, O.isSome))
+  );
+
+export const hasPhysicalAddress = (record: NewNotificationRecord) =>
+  pipe(
+    record.input.body.recipients,
+    RA.every((recipient) => pipe(recipient.physicalAddress, O.fromNullable, O.isSome))
+  );
+
+export const hasRegisteredLetterAsPhysicalDocumentType = (record: NewNotificationRecord) =>
+  record.input.body.physicalCommunicationType === PhysicalCommunicationTypeEnum.REGISTERED_LETTER_890;
+
+export const hasRecipientPaymentCreditorTaxId = (record: NewNotificationRecord) =>
+  pipe(
+    record.input.body.recipients,
+    RA.every((recipient) =>
+      pipe(
+        recipient.payment,
+        O.fromNullable,
+        O.map((_) => _.creditorTaxId),
+        O.isSome
+      )
+    )
+  );
+
+export const hasRecipientPaymentNoticeCode = (record: NewNotificationRecord) =>
+  pipe(
+    record.input.body.recipients,
+    RA.every((recipient) =>
+      pipe(
+        recipient.payment,
+        O.fromNullable,
+        O.map((_) => _.noticeCode),
+        O.isSome
+      )
+    )
+  );
 
 const getNotification = (record: NewNotificationRecord): O.Option<Notification> =>
   record.output.statusCode === 202 ? O.some({ ...record.input.body, ...record.output.returned }) : O.none;
