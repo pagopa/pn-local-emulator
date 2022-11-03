094f3ac9c234348d589bf94dbb9cffe8952f8ddb
Marco Comi <9998393+kin0992@users.noreply.github.com>
2022-11-03 15:14:26 +0100

    Compute sha256 of uploaded document

--- src/useCases/UploadToS3UseCase.ts
+++ src/useCases/UploadToS3UseCase.ts
@@ -1,3 +1,5 @@
+import crypto from 'crypto';
+import * as Buffer from 'buffer';
 import { pipe } from 'fp-ts/lib/function';
 import * as TE from 'fp-ts/TaskEither';
 import { AmzChecksumSHA256 } from '../generated/definitions/AmzChecksumSHA256';
@@ -7,13 +9,16 @@ import { AmzVersionId } from '../generated/definitions/AmzVersionId';
 import { AmzDocumentKey } from '../generated/definitions/AmzDocumentKey';
 import { SystemEnv } from './SystemEnv';
 
+const computeSha256 = (bytes: Buffer) => crypto.createHash('sha256').update(bytes).digest('base64');
+
 export const UploadToS3UseCase =
   ({ uploadToS3RecordRepository }: SystemEnv) =>
   (key: AmzDocumentKey) =>
   (checksumAlg?: AmzSdkChecksumAlg) =>
   (secret: AmzMetaSecret) =>
-  (checksum: AmzChecksumSHA256): TE.TaskEither<Error, AmzVersionId> => {
-    const input = { key, checksumAlg, secret, checksum };
+  (checksum: AmzChecksumSHA256) =>
+  (documentAsBytes: Buffer): TE.TaskEither<Error, AmzVersionId> => {
+    const input = { key, checksumAlg, secret, checksum, computedSha256: computeSha256(documentAsBytes) };
     const output = { statusCode: 200 as const, returned: Math.random() };
     return pipe(
       uploadToS3RecordRepository.insert({ type: 'UploadToS3Record', input, output }),
