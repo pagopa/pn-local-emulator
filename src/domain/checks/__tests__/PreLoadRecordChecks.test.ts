06960df34bdf18908965bb8917f412115fd1e1ae
Marco Comi <9998393+kin0992@users.noreply.github.com>
2022-11-09 10:58:40 +0100

    Changes on PR #72 (#74)

--- src/domain/checks/__tests__/PreLoadRecordChecks.test.ts
+++ src/domain/checks/__tests__/PreLoadRecordChecks.test.ts
@@ -1,30 +1,26 @@
 import * as PreLoadRecordChecks from '../PreLoadRecordChecks';
-import * as data from '../../__tests__/data';
-
-// TODO: Refactor using fast-check
-const ex0 = [data.preLoadRecord];
-const ex1 = [data.preLoadRecord, data.preLoadRecord];
+import * as useCaseData from './data';
 
 describe('PreLoadRecordChecks', () => {
   it('atLeastOnePreLoadRecordC', () => {
     const check = PreLoadRecordChecks.atLeastOnePreLoadRecordC;
     expect(check([])).toStrictEqual(false);
-    expect(check(ex0)).toStrictEqual(true);
+    expect(check(useCaseData.preLoadRecordSingletonList)).toStrictEqual(true);
   });
   it('atLeastOnePreLoadRecordWithPdfC', () => {
     const check = PreLoadRecordChecks.atLeastOnePreLoadRecordWithPdfC;
     expect(check([])).toStrictEqual(false);
-    expect(check(ex0)).toStrictEqual(true);
+    expect(check(useCaseData.preLoadRecordSingletonList)).toStrictEqual(true);
   });
   it('atLeastOneValidSlotC', () => {
     const check = PreLoadRecordChecks.atLeastOneValidSlotC;
     expect(check([])).toStrictEqual(false);
-    expect(check(ex0)).toStrictEqual(true);
+    expect(check(useCaseData.preLoadRecordSingletonList)).toStrictEqual(true);
   });
   it('atLeastTwoValidSlotC', () => {
     const check = PreLoadRecordChecks.atLeastTwoValidSlotC;
     expect(check([])).toStrictEqual(false);
-    expect(check(ex0)).toStrictEqual(false);
-    expect(check(ex1)).toStrictEqual(true);
+    expect(check(useCaseData.preLoadRecordSingletonList)).toStrictEqual(false);
+    expect(check(useCaseData.preLoadRecords)).toStrictEqual(true);
   });
 });
