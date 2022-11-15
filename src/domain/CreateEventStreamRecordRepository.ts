1adfc3d540e3638fbd88840055de56dfff4a4b31
Marco Comi <9998393+kin0992@users.noreply.github.com>
2022-11-15 18:54:17 +0100

    Add predicates

--- src/domain/CreateEventStreamRecordRepository.ts
+++ src/domain/CreateEventStreamRecordRepository.ts
@@ -1,7 +1,8 @@
+import * as O from 'fp-ts/Option';
 import { ApiKey } from '../generated/definitions/ApiKey';
-import { StreamCreationRequest } from '../generated/streams/StreamCreationRequest';
+import { EventTypeEnum, StreamCreationRequest } from '../generated/streams/StreamCreationRequest';
 import { StreamMetadataResponse } from '../generated/streams/StreamMetadataResponse';
-import { Repository } from './Repository';
+import { AllRecord, Repository } from './Repository';
 import { Response, UnauthorizedMessageBody } from './types';
 
 export type CreateEventStreamRecord = {
@@ -11,3 +12,12 @@ export type CreateEventStreamRecord = {
 };
 
 export type CreateEventStreamRecordRepository = Repository<CreateEventStreamRecord>;
+
+export const isCreateEventStreamRecord = (record: AllRecord): O.Option<CreateEventStreamRecord> =>
+  record.type === 'CreateEventStreamRecord' ? O.some(record) : O.none;
+
+// TODO: Add missing unit tests
+export const hasTimelineEventTypeToTimeline = (record: CreateEventStreamRecord) =>
+  record.input.body.eventType === EventTypeEnum.TIMELINE;
+
+export const isSuccessfulResponse = (record: CreateEventStreamRecord) => record.output.statusCode === 200;
