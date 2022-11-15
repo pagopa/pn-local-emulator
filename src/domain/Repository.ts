b043c5d5b22b036a513df8ffe22f1597573a95bb
Marco Comi <9998393+kin0992@users.noreply.github.com>
2022-11-15 15:33:46 +0100

    Add CreateEventStreamRecord to AllRecord

--- src/domain/Repository.ts
+++ src/domain/Repository.ts
@@ -5,6 +5,7 @@ import { ApiKey } from '../generated/definitions/ApiKey';
 import { NewNotificationRecord } from './NewNotificationRepository';
 import { PreLoadRecord } from './PreLoadRepository';
 import { UploadToS3Record } from './UploadToS3RecordRepository';
+import { CreateEventStreamRecord } from './CreateEventStreamRecordRepository';
 
 export type Repository<A> = {
   insert: (input: A) => TE.TaskEither<Error, A>;
@@ -13,7 +14,7 @@ export type Repository<A> = {
 };
 
 // TODO: Add missing records or find another solution
-export type AllRecord = PreLoadRecord | UploadToS3Record | NewNotificationRecord;
+export type AllRecord = PreLoadRecord | UploadToS3Record | NewNotificationRecord | CreateEventStreamRecord;
 
 export const existsApiKey = <T extends { input: { apiKey: ApiKey } }>(record: T) =>
   pipe(record.input.apiKey, O.fromNullable, O.isSome);
