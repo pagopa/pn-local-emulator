4e3ae09ccbdd70367f65a2af6fe4088143ab4480
Marco Comi <9998393+kin0992@users.noreply.github.com>
2022-11-15 18:54:37 +0100

    Add new data

--- src/domain/checks/__tests__/data.ts
+++ src/domain/checks/__tests__/data.ts
@@ -1,4 +1,6 @@
 import * as data from '../../__tests__/data';
+import { EventTypeEnum } from '../../../generated/streams/StreamCreationRequest';
+import {unauthorizedResponse} from "../../types";
 
 // TODO: Refactor using fast-check
 export const preLoadRecordSingletonList = [data.preLoadRecord];
@@ -19,4 +21,22 @@ export const evenPreLoadAndUploadRecordsThatAreNotLinked = [
   data.preLoadRecord,
   data.uploadToS3Record,
   data.uploadToS3RecordDangling,
+];
+
+export const createEventStreamRecordWithoutEventType = [data.createEventStreamRecord];
+export const createEventStreamRecordWithEventTypeTimeline = [
+  {
+    ...data.createEventStreamRecord,
+    input: {
+      ...data.createEventStreamRecord.input,
+      body: { ...data.createEventStreamRecord.input.body, eventType: EventTypeEnum.TIMELINE },
+    },
+  },
+];
+
+export const failedRequestCreateEventStream = [
+  {
+    ...createEventStreamRecordWithEventTypeTimeline[0],
+    output: unauthorizedResponse,
+  }
 ];
