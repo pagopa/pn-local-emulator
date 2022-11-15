a3eec7dd817ca3e10d5af9b5f803ba20847d96ec
Marco Comi <9998393+kin0992@users.noreply.github.com>
2022-11-15 15:46:43 +0100

    Create the list of checks related to the tcSend02 useCase

---
+++ src/domain/checks/tcSend02.ts
@@ -0,0 +1,16 @@
+import { Group } from '../reportengine/reportengine';
+
+// This is here just to let it compile. Once the implementation has been completed, this line can be removed.
+const mockCheck = () => false;
+
+export const tcSend02 = Group({
+  'Configure a stream where you can get the timeline events of a notification': Group({
+    'Have you created the stream with the property "eventType" set to "TIMELINE"?': mockCheck,
+  }),
+  'Consume events from a stream': Group({
+    'Have you make a request to get the stream with the streamId provided during the creation of the stream?':
+      mockCheck,
+    'Have you honored the "retryAfter" value?': mockCheck,
+    'Have you received the event with status "ACCEPTED"?': mockCheck,
+  }),
+});
