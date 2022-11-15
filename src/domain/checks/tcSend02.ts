653e7c3ab876ee560683028181924bc3ab14e73e
Marco Comi <9998393+kin0992@users.noreply.github.com>
2022-11-15 18:54:54 +0100

    Replace first mock check with real implementation

--- src/domain/checks/tcSend02.ts
+++ src/domain/checks/tcSend02.ts
@@ -1,11 +1,13 @@
 import { Group } from '../reportengine/reportengine';
+import * as CreateEventStreamRecordChecks from './CreateEventStreamRecordChecks';
 
 // This is here just to let it compile. Once the implementation has been completed, this line can be removed.
 const mockCheck = () => false;
 
 export const tcSend02 = Group({
   'Configure a stream where you can get the timeline events of a notification': Group({
-    'Have you created the stream with the property "eventType" set to "TIMELINE"?': mockCheck,
+    'Have you created the stream with the property "eventType" set to "TIMELINE"?':
+      CreateEventStreamRecordChecks.hasCreateStreamWithEventTypeTimeline,
   }),
   'Consume events from a stream': Group({
     'Have you make a request to get the stream with the streamId provided during the creation of the stream?':
