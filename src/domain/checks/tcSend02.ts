bcab3b5d8a5f5c7d45c23f00f1a1f485b6a33765
Marco Comi <9998393+kin0992@users.noreply.github.com>
2022-11-24 09:57:15 +0100

    Improve report for tcSend02

--- src/domain/checks/tcSend02.ts
+++ src/domain/checks/tcSend02.ts
@@ -7,14 +7,17 @@ const mockCheck = () => false;
 
 export const tcSend02 = Group({
   'Configure a stream where you can get the timeline events of a notification': Group({
-    'Have you created the stream with the property "eventType" set to "TIMELINE"?':
+    'Have you created the stream with the property eventType set to TIMELINE?':
       CreateEventStreamRecordChecks.hasCreatedStreamWithEventTypeTimelineC,
   }),
   'Consume events from a stream': Group({
     'Have you make a request to get the stream with the streamId provided during the creation of the stream?':
       ConsumeEventStreamRecordChecks.requestWithStreamIdProvidedHasBeenMadeC,
-    'Have you honored the "retryAfter" value?': mockCheck,
-    'Have you received the event with status "ACCEPTED"?':
-      ConsumeEventStreamRecordChecks.hasReceivedEventWithStatusAcceptedC,
+    'Have you received the event that confirms the creation of the notification?': Group({
+      'Have you received the event with newStatus set to ACCEPTED?':
+        ConsumeEventStreamRecordChecks.hasNewStatusPropertySetToAcceptedC,
+    }),
+    // 'Have you honored the retry-after value?': mockCheck,
+    'Have you properly consumed the events coming from the stream?': mockCheck,
   }),
 });
