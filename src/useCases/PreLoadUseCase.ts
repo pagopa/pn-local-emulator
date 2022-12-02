d8a66b19411a9651005ae89816834ccd35ebd803
AF <alessandro.ferlin@pagopa.it>
2022-12-02 11:10:57 +0100

    Remove missing type imported from definitions/*

--- src/useCases/PreLoadUseCase.ts
+++ src/useCases/PreLoadUseCase.ts
@@ -1,14 +1,12 @@
 import { pipe } from 'fp-ts/lib/function';
 import * as TE from 'fp-ts/TaskEither';
 import { makePreLoadRecord, PreLoadRecord } from '../domain/PreLoadRepository';
-import { ApiKey } from '../generated/definitions/ApiKey';
-import { PreLoadRequestBody } from '../generated/definitions/PreLoadRequestBody';
 import { SystemEnv } from './SystemEnv';
 
 export const PreLoadUseCase =
   (env: SystemEnv) =>
-  (apiKey: ApiKey) =>
-  (body: PreLoadRequestBody): TE.TaskEither<Error, PreLoadRecord['output']> =>
+  (apiKey: PreLoadRecord['input']['apiKey']) =>
+  (body: PreLoadRecord['input']['body']): TE.TaskEither<Error, PreLoadRecord['output']> =>
     pipe(
       makePreLoadRecord({ ...env, input: { apiKey, body } }),
       env.recordRepository.insert,
