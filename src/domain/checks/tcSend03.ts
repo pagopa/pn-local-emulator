028e83deed2407af207715ef6140447dce8c0020
Marco Comi <9998393+kin0992@users.noreply.github.com>
2022-12-19 17:57:42 +0100

    Update tcSend03 check structure

--- src/domain/checks/tcSend03.ts
+++ src/domain/checks/tcSend03.ts
@@ -1,8 +1,16 @@
 import { Group } from '../reportengine/reportengine';
+import * as GetNotificationDocumentMetadataChecks from './GetNotificationDocumentMetadataChecks';
 
 export const tcSend03 = Group({
-  'Have you downloaded the documents related to a notification?': Group({
-    'Have you downloaded the notification PDF?': () => false,
-    'Have you downloaded the pagoPA PDF?': () => false,
+  'Download documents attached to the notification': Group({
+    'Have you downloaded the notified document?': Group({
+      'Have you requested the metadata of the notification with the IUN retrieved previously?':
+        GetNotificationDocumentMetadataChecks.getNotificationDocumentMetadataC,
+      'Have you downloaded the PDF of the notification?': () => false,
+    }),
+    'Have you downloaded the pagoPA payment document?': Group({
+      'Have you requested the metadata of the payment document?': () => false,
+      'Have you downloaded the PDF of the payment document?': () => false,
+    }),
   }),
 });
