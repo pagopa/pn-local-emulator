8419c6b50e402ee750fc60dadd76fb025e9e6685
AF <alessandro.ferlin@pagopa.it>
2023-01-09 17:55:48 +0100

    Fix typo

--- src/domain/checks/tcPayment01.ts
+++ src/domain/checks/tcPayment01.ts
@@ -1,7 +1,7 @@
 import { Group } from '../reportengine/reportengine';
 
 export const tcPayment01 = Group({
-  'Create a notification request providing the same sender and creator': Group({
+  'Create a notification request providing the same sender and creditor': Group({
     'Have you filled at least one recipient where the field payment.creditorTaxId is the same as the senderTaxId?':
       () => true,
   }),
