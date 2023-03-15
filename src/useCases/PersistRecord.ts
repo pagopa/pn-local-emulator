b3e9a5daa99bc0de3861f75f02b1abe46e0b8a5e
AF <alessandro.ferlin@pagopa.it>
2023-03-15 09:07:21 +0100

    Fix the value of 'x-amz-version-id'
    Before this commit, the value returned as x-amz-version-id was wrong.
    After this commit the value returned is correct.

--- src/useCases/PersistRecord.ts
+++ src/useCases/PersistRecord.ts
@@ -3,5 +3,7 @@ import * as TE from 'fp-ts/TaskEither';
 import { Record } from '../domain/Repository';
 import { SystemEnv } from './SystemEnv';
 
-export const persistRecord = (env: SystemEnv) => (fn: (list: ReadonlyArray<Record>) => Record) =>
-  pipe(env.recordRepository.list(), TE.map(fn), TE.chain(env.recordRepository.insert));
+export const persistRecord =
+  (env: SystemEnv) =>
+  <R extends Record>(fn: (list: ReadonlyArray<Record>) => R) =>
+    pipe(env.recordRepository.list(), TE.map(fn), TE.chain(env.recordRepository.insert));
