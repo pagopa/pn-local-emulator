a440fa10e9a5f65a880002a9b8df5eeeb6c4f936
Cosmin Constantin Tuduran <cosminconstantin.tuduran@nttdata.com>
2023-09-21 16:17:40 +0200

    Feature/VL-278 (#16)
    * VL-278 - corrected response of payments download
    
    ---------
    
    Co-authored-by: CCTuduran <140727607+CCTuduran@users.noreply.github.com>
    Co-authored-by: dennycarusonttdata <denny.caruso@nttdata.com>
    Co-authored-by: Alexandru Daniel Iova <alexandrudaniel.iova@emeal.nttdata.com>

--- src/domain/GetPaymentNotificationMetadataRecord.ts
+++ src/domain/GetPaymentNotificationMetadataRecord.ts
@@ -54,7 +54,11 @@ export const makeGetPaymentNotificationMetadataRecord =
         pipe(
           computeSnapshot(env)(records),
           RA.filterMap(O.fromEither),
-          RA.chain((notification) => (notification.iun === input.iun ? notification.recipients : RA.empty)),
+          // Per il momento solo il tipo PAGOPA è gestito. In caso di valori differenti si restituisce 404.
+          RA.filter((notification) => notification.iun && input.attachmentName === 'PAGOPA'),
+          // Si verifica che il recipientId è coerente e presente negli array dei recipients. In caso contrario restituiamo 404.
+          RA.filter((notification) => input.recipientId >= 0 && notification.recipients.length > input.recipientId),
+          RA.chain((notification) => (notification.iun === input.iun ? [notification.recipients[input.recipientId]] : RA.empty)),
           RA.filterMap((recipient) => O.fromNullable(recipient.payment)),
           RA.findLastMap(getNotificationPaymentAttachment(input.attachmentName)),
           O.map(makeNotificationAttachmentDownloadMetadataResponse(env)),
