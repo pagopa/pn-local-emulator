0d7989f642db3c23e2f4019769555ec7ee8e28b7
dennycarusonttdata <141228124+dennycarusonttdata@users.noreply.github.com>
2023-11-21 10:19:03 +0100

    merge main from features/2.0/main (#14)
    * generated yaml from version 2.1
    
    * commented 'F24Metadata' schema inside yaml, due error generating models
    
    * updated 'hot-to-generate-the...md'
    
    * completed VL-295
    
    * eslint fix
    
    * eslint fix
    
    * changed NewNotificationRequest to NewNotificationRequestV21. Now there are errors before running
    
    * changed NewNotificationRequest to NewNotificationRequestV21. Now there are errors before running
    
    * fix file with errors after using NewNotificationRequestV21
    
    * fix file with errors after using NewNotificationRequestV21
    
    * eslint fix
    
    * completed again VL-295
    
    * completed again VL-295
    
    * completed again VL-295
    
    * still fix to do
    
    * added todo denny
    
    * first todo denny fix completed
    
    * eslint fix
    
    * fix for VL-295 due to issue in other files
    
    * eslint suggested fixes
    
    * eslint suggested fixes
    
    * fix for VL-295 w/ Cosmin
    
    * eslint fix
    
    * eslint fix
    
    * eslint fix
    
    * changing old type to the new ones
    
    * eslint fix
    
    * eslint fix
    
    * eslint fix
    
    * removed unused enum
    
    * fix accepted issue
    
    * fix tslint issues
    
    * fix tslint issue
    
    * Features/2.1/main (#12)
    
    * partial permit upload of json during uploadS3
    
    * partial permit upload of json during uploadS3
    
    * partial permit upload of json during uploadS3
    
    * handled attachmentName 'F24'
    
    * completed VL-294 + fix critical severity vulnerability in package-lock.json raised by npm install command
    
    * added semicolon due to husky error
    
    * eslint fix
    
    * fix reference to delivery-push url
    
    * fix
    
    * fix imports
    
    * fix req 7 handling
    
    * fix req 7 handling
    
    * update package-lock
    
    * completed VL-296
    
    * completed VL-296
    
    * VL-298 completed
    
    * VL-299 completed
    
    * added collection json + 2.1 version
    
    ---------
    
    Co-authored-by: Cosmin15 <Tuduran.cosmin@gmail.com>
    Co-authored-by: Tuduran, Cosmin Constantin <cosminconstantin.tuduran@nttdata.com>
    
    * Features/2.1/main (#13)
    
    * partial permit upload of json during uploadS3
    
    * partial permit upload of json during uploadS3
    
    * partial permit upload of json during uploadS3
    
    * handled attachmentName 'F24'
    
    * completed VL-294 + fix critical severity vulnerability in package-lock.json raised by npm install command
    
    * added semicolon due to husky error
    
    * eslint fix
    
    * fix reference to delivery-push url
    
    * fix
    
    * fix imports
    
    * fix req 7 handling
    
    * fix req 7 handling
    
    * update package-lock
    
    * completed VL-296
    
    * completed VL-296
    
    * VL-298 completed
    
    * VL-299 completed
    
    * added collection json + 2.1 version
    
    * update tests
    
    * fix tests
    
    * fix tests
    
    ---------
    
    Co-authored-by: Cosmin15 <Tuduran.cosmin@gmail.com>
    Co-authored-by: Tuduran, Cosmin Constantin <cosminconstantin.tuduran@nttdata.com>
    
    * fix tests
    
    ---------
    
    Co-authored-by: Cosmin Constantin Tuduran <cosminconstantin.tuduran@nttdata.com>
    Co-authored-by: Cosmin15 <Tuduran.cosmin@gmail.com>

--- src/adapters/http/updateEventStream/router.ts
+++ src/adapters/http/updateEventStream/router.ts
@@ -1,35 +1,34 @@
-import { flow, pipe } from 'fp-ts/function';
+import express from 'express';
 import * as Apply from 'fp-ts/Apply';
 import * as E from 'fp-ts/Either';
-import * as t from 'io-ts';
-import * as TE from 'fp-ts/TaskEither';
 import * as T from 'fp-ts/Task';
-import express from 'express';
-import { traceWithValue } from 'fp-ts-std/Debug';
-import { updateStreamRecordReturningOnlyTheOneUpdatedStream } from '../../../useCases/PersistRecord';
-import { Handler, toExpressHandler } from '../Handler';
-import { SystemEnv } from '../../../useCases/SystemEnv';
-import * as Problem from '../Problem';
+import * as TE from 'fp-ts/TaskEither';
+import { flow, pipe } from 'fp-ts/function';
+import * as t from 'io-ts';
 import { makeUpdateStreamRecord } from '../../../domain/UpdateStreamRecord';
 import { StreamCreationRequest } from '../../../generated/streams/StreamCreationRequest';
+import { updateStreamRecordReturningOnlyTheOneUpdatedStream } from '../../../useCases/PersistRecord';
+import { SystemEnv } from '../../../useCases/SystemEnv';
+import { Handler, toExpressHandler } from '../Handler';
+import * as Problem from '../Problem';
 
 const handler =
   (env: SystemEnv): Handler =>
-  (req, res) =>
-    pipe(
-      Apply.sequenceS(E.Apply)({
-        apiKey: t.string.decode(req.headers['x-api-key']),
-        body: StreamCreationRequest.decode(req.body),
-        streamId: t.string.decode(req.params.streamId),
-      }),
-      E.map(flow(makeUpdateStreamRecord(env), traceWithValue("aaa: "), updateStreamRecordReturningOnlyTheOneUpdatedStream(env))),
-      E.map(
-        TE.fold(
-          (_) => T.of(res.status(404).send(Problem.fromNumber(404))),
-          ({ output }) => T.of(res.status(output.statusCode).send(output.returned))
+    (req, res) =>
+      pipe(
+        Apply.sequenceS(E.Apply)({
+          apiKey: t.string.decode(req.headers['x-api-key']),
+          body: StreamCreationRequest.decode(req.body),
+          streamId: t.string.decode(req.params.streamId),
+        }),
+        E.map(flow(makeUpdateStreamRecord(env), updateStreamRecordReturningOnlyTheOneUpdatedStream(env))),
+        E.map(
+          TE.fold(
+            (_) => T.of(res.status(404).send(Problem.fromNumber(404))),
+            ({ output }) => T.of(res.status(output.statusCode).send(output.returned))
+          )
         )
-      )
-    );
+      );
 
 export const makeUpdateEventStreamRouter = (env: SystemEnv): express.Router => {
   const router = express.Router();
