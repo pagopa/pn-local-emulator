44133f6ab1af3b7fbeadbb38ca9afd2d21ce13ef
dennycarusonttdata <141228124+dennycarusonttdata@users.noreply.github.com>
2023-12-07 11:05:55 +0100

    From NTT Main to PagoPa Main (#155)
    Co-authored-by: Cosmin Constantin Tuduran <Denny.Caruso@nttdata.com>
    Co-authored-by: Cosmin Constantin Tuduran <cosminconstantin.tuduran@nttdata.com>

--- src/domain/TimelineElementCancelledNotificationTimelineEvents.ts
+++ src/domain/TimelineElementCancelledNotificationTimelineEvents.ts
@@ -1,27 +1,17 @@
 /* eslint-disable max-lines-per-function */
 
-import { pipe } from 'fp-ts/function';
-import * as O from 'fp-ts/Option';
-import * as RA from 'fp-ts/ReadonlyArray';
 import { DigitalAddressSourceEnum } from '../generated/pnapi/DigitalAddressSource';
 import { FullSentNotificationV21 } from '../generated/pnapi/FullSentNotificationV21';
 import { LegalFactCategoryEnum } from '../generated/pnapi/LegalFactCategory';
 import { NotificationRecipient } from '../generated/pnapi/NotificationRecipient';
 import { TimelineElementV20 } from '../generated/pnapi/TimelineElementV20';
-import { NotificationStatusHistory } from '../generated/pnapi/NotificationStatusHistory';
-import { NotificationStatusEnum } from '../generated/pnapi/NotificationStatus';
 import { IUNGeneratorByIndex } from '../adapters/randexp/IUNGenerator';
 import { TimelineElementCategoryV20Enum } from '../generated/pnapi/TimelineElementCategoryV20';
-import { Notification } from './Notification';
 import { DomainEnv } from './DomainEnv';
-import { makeTimeLineListPEC_CancelledNotification } from './TimelineElementCancelledNotificationTimelineEvents';
 
-
-const makeTimelineListPEC =
-  (env: DomainEnv) =>
-  (notification: FullSentNotificationV21) =>
-  (index: number, recipient: NotificationRecipient): ReadonlyArray<TimelineElementV20> =>
-    notification.notificationStatus !== NotificationStatusEnum.CANCELLED ? [
+export const makeTimeLineListPEC_CancelledNotification = 
+    (env: DomainEnv, notification: FullSentNotificationV21, index: number, recipient: NotificationRecipient) : ReadonlyArray<TimelineElementV20> =>
+    [
       {
         elementId: `${notification.iun}_aar_gen_${index}`,
         timestamp: env.dateGenerator(),
@@ -33,6 +23,7 @@ const makeTimelineListPEC =
           generatedAarUrl: `safestorage://PN_AAR-0002-${env.iunGenerator()}`,
         },
       },
+
       {
         elementId: `${notification.iun}_get_address_${index}_source_PLATFORM_attempt_0`,
         timestamp: env.dateGenerator(),
@@ -45,6 +36,7 @@ const makeTimelineListPEC =
           attemptDate: env.dateGenerator(),
         },
       },
+
       {
         elementId: `${notification.iun}_get_address_${index}_source_SPECIAL_attempt_0`,
         timestamp: env.dateGenerator(),
@@ -57,6 +49,7 @@ const makeTimelineListPEC =
           attemptDate: env.dateGenerator(),
         },
       },
+
       {
         elementId: `${notification.iun}_send_digital_domicile_${index}_source_SPECIAL_attempt_0`,
         timestamp: env.dateGenerator(),
@@ -69,6 +62,7 @@ const makeTimelineListPEC =
           retryNumber: 0,
         },
       },
+
       {
         elementId: `${notification.iun}_digital_delivering_progress_${index}_source_SPECIAL_attempt_0_progidx_1`,
         timestamp: env.dateGenerator(),
@@ -89,6 +83,7 @@ const makeTimelineListPEC =
           shouldRetry: false,
         },
       },
+
       {
         elementId: `${notification.iun}_send_digital_feedback_${index}_source_SPECIAL_attempt_0`,
         timestamp: env.dateGenerator(),
@@ -108,6 +103,7 @@ const makeTimelineListPEC =
           sendingReceipts: [{}],
         },
       },
+
       {
         elementId: `${notification.iun}_digital_success_workflow_${index}`,
         timestamp: env.dateGenerator(),
@@ -123,6 +119,7 @@ const makeTimelineListPEC =
           digitalAddress: recipient.digitalDomicile,
         },
       },
+
       {
         elementId: `${notification.iun}_schedule_refinement_workflow_${index}`,
         timestamp: env.dateGenerator(),
@@ -132,6 +129,7 @@ const makeTimelineListPEC =
           recIndex: index,
         },
       },
+
       {
         elementId: `${notification.iun}_refinement_${index}`,
         timestamp: env.dateGenerator(),
@@ -142,6 +140,7 @@ const makeTimelineListPEC =
           notificationCost: 100,
         },
       },
+
       {
         elementId: `${notification.iun}_notification_viewed_${index}`,
         timestamp: env.dateGenerator(),
@@ -156,50 +155,25 @@ const makeTimelineListPEC =
           recIndex: index,
         },
       },
-    ] : makeTimeLineListPEC_CancelledNotification(env, notification, index, recipient);
 
-export const makeTimelineList =
-  (env: DomainEnv) =>
-  (notification: FullSentNotificationV21): ReadonlyArray<TimelineElementV20> =>
-    [
       {
-        elementId: `${notification.iun}_request_accepted`,
+        elementId: `NOTIFICATION_CANCELLATION_REQUEST.IUN_${notification.iun}`,
         timestamp: env.dateGenerator(),
-        legalFactsIds: [
-          {
-            key: `safestorage://PN_LEGAL_FACTS-0002-${IUNGeneratorByIndex(notification.iun, 0)}`,
-            category: LegalFactCategoryEnum.SENDER_ACK,
-          },
-        ],
-        category: TimelineElementCategoryV20Enum.REQUEST_ACCEPTED,
+        legalFactsIds: [],
+        category: TimelineElementCategoryV20Enum.NOTIFICATION_CANCELLATION_REQUEST,
+        details: {
+          cancellationRequestId: "90e3f130-cb23-4b6b-a0aa-858de7ffb3a0"
+        }
       },
-      ...pipe(notification.recipients, RA.chainWithIndex(makeTimelineListPEC(env)(notification))),
-    ];
-
-export const makeNotificationStatusHistory =
-  (env: DomainEnv) =>
-  (notificationStatus: NotificationStatusEnum, timeline: ReadonlyArray<TimelineElementV20>): NotificationStatusHistory =>
-    [
+      
       {
-        status: notificationStatus,
-        activeFrom: pipe(
-          timeline,
-          RA.head,
-          O.fold(env.dateGenerator, ({ timestamp }) => timestamp || env.dateGenerator())
-        ),
-        relatedTimelineElements: pipe(
-          timeline,
-          RA.map(({ elementId }) => elementId || '')
-        ),
-      },
+        elementId: `NOTIFICATION_CANCELLED.IUN_${notification.iun}`,
+        timestamp: env.dateGenerator(),
+        legalFactsIds: [],
+        category: TimelineElementCategoryV20Enum.NOTIFICATION_CANCELLED,
+        details: {
+          notificationCost: 100,
+          notRefinedRecipientIndexes: [0]
+        }
+      }
     ];
-
-export const updateTimeline =
-  (env: DomainEnv) =>
-  (notification: Notification, newNotificationStatus: NotificationStatusEnum): Notification =>
-    pipe(notification, makeTimelineList(env), (timelineList) => ({
-      ...notification,
-      notificationStatus: newNotificationStatus,
-      notificationStatusHistory: makeNotificationStatusHistory(env)(newNotificationStatus, timelineList),
-      timeline: timelineList,
-    }));
