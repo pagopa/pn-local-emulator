79b194fb0f559c133222f25397f7a4e5c9ddfe1e
MicheleBellizziNTT <MicheleGiorgio.Bellizzi@nttdata.com>
2025-09-08 16:53:04 +0200

    aggiornamento2.5

--- src/domain/checks/tcInvio04.ts
+++ src/domain/checks/tcInvio04.ts
@@ -4,20 +4,6 @@ import * as GetPaymentNotificationMetadataChecks from './GetPaymentNotificationM
 import * as LegalFactDownloadMetadataChecks from './LegalFactDownloadMetadataChecks';
 
 export const tcInvio04 = Group({
-  'Download documents attached to the notification': Group({
-    'Have you downloaded the notified document?': Group({
-      'Have you requested the metadata of the notification with the IUN retrieved previously?':
-        GetNotificationDocumentMetadataChecks.getNotificationDocumentMetadataC,
-      'Have you downloaded the PDF of the notification?':
-        GetNotificationDocumentMetadataChecks.downloadedNotificationDocumentC,
-    }),
-    'Have you downloaded the pagoPA payment document?': Group({
-      'Have you requested the metadata of the pagoPA payment attachment with the IUN retrieved previously?':
-        GetPaymentNotificationMetadataChecks.matchesIunAndHasPAGOPAAsAttachmentName,
-      'Have you downloaded the PDF of the pagoPA payment document?':
-        GetPaymentNotificationMetadataChecks.downloadedPaymentDocumentC,
-    }),
-  }),
   'Have you downloaded the legal facts?': Group({
     'Have you requested the metadata of the legal facts?':
       LegalFactDownloadMetadataChecks.getLegalFactDownloadMetadataRecord,
